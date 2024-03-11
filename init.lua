970733952d73c492af88aa72e6e8987e797fbfd2
Corwin Marsh <corwinm@users.noreply.github.com>
2024-03-11 00:07:23 -0700

    Update which-key config

--- init.lua
+++ init.lua
@@ -370,6 +370,7 @@ require('lazy').setup({
       -- Document existing key chains
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>sd'] = { name = '[S]earch [D]ocument', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
