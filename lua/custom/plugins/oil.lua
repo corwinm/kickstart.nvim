1e6393beb19b2def5cffd826d24ef7eff4e0bf9f
Corwin Marsh <corwinm@users.noreply.github.com>
2024-06-02 18:05:22 -0700

    Turn off auto-save when oil is open

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -19,6 +19,20 @@ return {
 
       -- Open parent directory in floating window
       vim.keymap.set('n', '<leader>-', require('oil').toggle_float, { desc = 'Open parent directory - float' })
+
+      -- Enable and disable auto-save when opening and leaving oil
+      vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+        pattern = { 'oil://*' },
+        callback = function()
+          require('auto-save').off()
+        end,
+      })
+      vim.api.nvim_create_autocmd({ 'BufLeave' }, {
+        pattern = { 'oil://*' },
+        callback = function()
+          require('auto-save').on()
+        end,
+      })
     end,
   },
 }
