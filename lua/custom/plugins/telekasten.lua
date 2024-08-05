d3bf52ebd10a50f76a439a25096c8e6eff4897df
Corwin Marsh <corwinm@users.noreply.github.com>
2024-08-04 22:47:37 -0700

    Fix markdown formatting and add some keymaps

--- lua/custom/plugins/telekasten.lua
+++ lua/custom/plugins/telekasten.lua
@@ -6,8 +6,9 @@ return {
       home = vim.fn.expand '~/notes',
       template_new_note = vim.fn.expand '~/.config/nvim/lua/custom/plugins/telekasten/new-note.md',
     }
+    vim.treesitter.language.register('markdown', 'telekasten')
     -- Launch panel if nothing is typed after <leader>z
-    vim.keymap.set('n', '<leader>,', '<cmd>Telekasten panel<CR>')
+    vim.keymap.set('n', '<leader>,,', '<cmd>Telekasten panel<CR>')
 
     -- Most used functions
     vim.keymap.set('n', '<leader>,f', '<cmd>Telekasten find_notes<CR>')
@@ -18,6 +19,10 @@ return {
     vim.keymap.set('n', '<leader>,c', '<cmd>Telekasten show_calendar<CR>')
     vim.keymap.set('n', '<leader>,b', '<cmd>Telekasten show_backlinks<CR>')
     vim.keymap.set('n', '<leader>,I', '<cmd>Telekasten insert_img_link<CR>')
+    vim.keymap.set('n', '<leader>,t', ':Telekasten toggle_todo<CR>')
+    vim.keymap.set('v', '<leader>,t', function()
+      require('telekasten').toggle_todo { v = true }
+    end)
 
     -- Call insert link automatically when we start typing a link
     vim.keymap.set('i', '[[', '<cmd>Telekasten insert_link<CR>')
