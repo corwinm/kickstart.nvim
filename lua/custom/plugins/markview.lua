8b867e6019d96052b4eddb83678fe9566983bad0
Corwin Marsh <corwinm@users.noreply.github.com>
2024-08-06 23:03:28 -0700

    Configure markview hybrid mode

--- lua/custom/plugins/markview.lua
+++ lua/custom/plugins/markview.lua
@@ -1,6 +1,10 @@
 return {
   'OXY2DEV/markview.nvim',
   lazy = false,
+  opts = {
+    modes = { 'i', 'n', 'no' },
+    hybrid_modes = { 'i' },
+  },
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
