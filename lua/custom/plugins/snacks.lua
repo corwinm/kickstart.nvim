6c052be12e6fd421047b47718173baec1e8d305b
Corwin Marsh <7642548+corwinm@users.noreply.github.com>
2025-11-08 18:26:59 -0800

    Add snacks.words keymaps

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -88,5 +88,21 @@ return {
       end,
       desc = 'GitHub Pull Requests (all)',
     },
+    {
+      ']]',
+      function()
+        Snacks.words.jump(vim.v.count1)
+      end,
+      desc = 'Next Reference',
+      mode = { 'n', 't' },
+    },
+    {
+      '[[',
+      function()
+        Snacks.words.jump(-vim.v.count1)
+      end,
+      desc = 'Prev Reference',
+      mode = { 'n', 't' },
+    },
   },
 }
