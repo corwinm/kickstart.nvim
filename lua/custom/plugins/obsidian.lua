7738a8400084f3faed201128c0cac557f5c2340a
Corwin Marsh <7642548+corwinm@users.noreply.github.com>
2025-11-14 19:42:03 -0800

    Dep updates and obsidian fixes

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -136,11 +136,9 @@ return {
     -- Either 'wiki' or 'markdown'.
     preferred_link_style = 'wiki',
 
-    -- Optional, boolean or a function that takes a filename and returns a boolean.
-    -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
-    disable_frontmatter = false,
-
     frontmatter = {
+      -- `false` indicates that you want obsidian.nvim to manage frontmatter.
+      enabled = true,
       func = function(note)
         -- Add the title of the note as an alias.
         if note.title then
@@ -209,14 +207,16 @@ return {
       },
     },
 
-    -- Optional, sort search results by "path", "modified", "accessed", or "created".
-    -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
-    -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
-    sort_by = 'modified',
-    sort_reversed = true,
-
-    -- Set the maximum number of lines to read from notes on disk when performing certain searches.
-    search_max_lines = 1000,
+    -- Optional, configure search settings
+    search = {
+      -- Sort search results by "path", "modified", "accessed", or "created".
+      -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
+      -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
+      sort_by = 'modified',
+      sort_reversed = true,
+      -- Set the maximum number of lines to read from notes on disk when performing certain searches.
+      max_lines = 1000,
+    },
 
     -- Optional, determines how certain commands open notes. The valid options are:
     -- 1. "current" (the default) - to always open in the current window
